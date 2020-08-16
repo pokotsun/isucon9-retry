a388a4bea7511ff4c00bccb4444fdb36daff0279
pokotsun <gorigolila@gmail.com>
2020-08-16 22:32:16 +0900

    removed unuse idx

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -77,6 +77,5 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `parent_id` int unsigned NOT NULL,
-  `category_name` varchar(191) NOT NULL,
-  INDEX idx_category (`category_id`)
+  `category_name` varchar(191) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
